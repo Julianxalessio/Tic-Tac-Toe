879fbcd332c0dd3facab8fae92c8805445301ac4
Diego Zwahlen <zwahlendiego@gmail.com>
2026-01-12 10:31:45 +0100

    Javadoc generated

---
+++ Server/MainGame.java
@@ -0,0 +1,65 @@
+package Server;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainGame {
+
+	public static void main(String[] args) throws Exception {
+		List<String[]> servers = new ArrayList<>();
+		int port = 6969;
+		DatagramSocket socket = new DatagramSocket(port);
+		System.out.println("ServerCreater Ready!");
+		boolean active = true;
+		while (active) {
+			byte[] buffer = new byte[1024];
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+
+			String msg = new String(packet.getData(), 0, packet.getLength()).trim();
+			System.out.println("Received: " + msg);
+			String[] msgParts = msg.split(";");
+			InetAddress sender = packet.getAddress();
+			if (msgParts.length == 3) {
+				if (msgParts[0].equals("main")) {
+					if (msgParts[1].equals("Create Server")) {
+						if (msgParts[2].matches("\\^[0-9]{4}$")) {
+							int items = servers.size();
+							for (String[] entry : servers) {
+								if (entry[0].equals(msgParts[2])) {
+									System.out.println("Server already Exists");
+//									Message Player that ID is already in use
+								} else
+									items--;
+							}
+							if (items <= 0) {
+								servers.add(new String[] { msgParts[2], sender.getHostAddress(), "" });
+							}
+						}
+					} else if (msgParts[1].equals("Join Server")) {
+						if (msgParts[2].matches("\\^[0-9]{4}$")) {
+							int items = servers.size();
+							for (int i = 0; i < servers.size(); i++) {
+								if (servers.get(i)[0].equals(msgParts[2])) {
+									servers.get(i)[2] = sender.getHostAddress();
+									Server server = new Server(servers.get(i)[1],servers.get(i)[2], socket);
+								} else
+									items--;
+							}
+							if (items <= 0) {
+//								Message Player Server not Found
+							}
+
+						}
+					}
+				} else {
+					System.out.println("");
+				}
+			}
+			socket.close();
+		}
+	}
+}
--- Server/Server.java
+++ Server/Server.java
@@ -1,94 +1,84 @@
+package Server;
+
 import java.io.IOException;
 import java.net.*;
- 
+
 /**
-* <h1>Server</h1>
-* <h6>This is the Server for the Tic Tac Toe Game</h6>
-* <h6><u>!!!Attention the IPs must be set to the IPs of the Clients!!!</u></h6>
-*
-* @version 1.0
-* @author Julian Lombardo
-* @author Diego Zwahlen
-* @author Lean Melone
-*/
+ * <h1>Server</h1>
+ * <h6>This is the Server for the Tic Tac Toe Game</h6>
+ * <h6><u>!!!Attention the IPs must be set to the IPs of the Clients!!!</u></h6>
+ *
+ * @version 1.0
+ * @author Julian Lombardo
+ * @author Diego Zwahlen
+ * @author Lean Melone
+ */
 public class Server {
     public static boolean playing = false;
     // Board als Array
     public static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
- 
+
     public static int time = 60;
-    
+
     public static boolean botPlaying = false;
-    
+
     public static InetAddress activePlayer;
-    
+
     public static boolean player1Ready = false, player2Ready = false;
- 
+
     /**
      * Mainfunction for the Server
-     * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
+    public Server(String player1IP, String player2IP, DatagramSocket socket) throws Exception {
 //      ------------------------Variables-------------------------
- 
-        // Playerlogic
- 
         int random = (int) (Math.random() * 2) + 1;
- 
+
         boolean currentPlayer = false; // false = player1 & true = player2
-        if (random == 1) {
-            currentPlayer = false;
-        } else {
-            currentPlayer = true;
-        }
- 
-        // Socket
-        int port = 6969;
-        DatagramSocket socket = new DatagramSocket(port);
- 
-        // Players
-        InetAddress player1 = InetAddress.getByName("192.168.1.93");
-        InetAddress player2 = InetAddress.getByName("192.168.1.92");
+        if (random == 1) currentPlayer = false;
+        else currentPlayer = true;
+
         botClass bot = new botClass();
-        // Gamelogic
- 
+        InetAddress player1 = InetAddress.getByName(player1IP);
+        InetAddress player2 = InetAddress.getByName(player2IP);
+
 //      ------------------------Game-------------------------
         System.out.println("TicTacToe Server ready");
         while (true) {
             while (!playing) {
                 botPlaying = false;
-                new Thread(() -> {
-                    while (time > -2) {
-                        if (time < 0) {
-						    activePlayer = player1Ready ? player1 : player2;
-						    playing = true;
-						    botPlaying = true;
-						    String stringBoard = boardToString(board);
-						    try {
-								sendMessageToPlayer(activePlayer, socket, stringBoard);
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						}
-                    }
-                }).start();
+
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
- 
+
                 String msg = new String(packet.getData(), 0, packet.getLength()).trim();
                 System.out.println("Received: " + msg);
- 
+
                 InetAddress sender = packet.getAddress();
- 
-                
+
+
                 if (sender.equals(player1) && msg.equals("yes")) {
                     player1Ready = true;
                     startTimer(player1Ready, player2Ready);
                 } else if (sender.equals(player2) && msg.equals("yes")) {
                     player2Ready = true;
                     startTimer(player1Ready, player2Ready);
+                }else if (sender.equals(player2) && msg.equals("yes_bot")) {
+                    activePlayer = player2;
+                    playing = true;
+                    botPlaying = true;
+                    botPlaying = true;
+                    String stringBoard = boardToString(board);
+                    sendMessageToPlayer(activePlayer, socket, stringBoard);
+                    break;
+                } else if (sender.equals(player1) && msg.equals("yes_bot")) {
+                    activePlayer = player1;
+                    playing = true;
+                    botPlaying = true;
+                    String stringBoard = boardToString(board);
+                    sendMessageToPlayer(activePlayer, socket, stringBoard);
+                    break;
                 }
                 if (player1Ready && player2Ready) {
                     playing = true;
@@ -111,21 +101,21 @@ public class Server {
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
- 
+
                 String msg = new String(packet.getData(), 0, packet.getLength()).trim();
                 System.out.println("Received: " + msg);
- 
+
                 // Spieler anhand der Absenderadresse bestimmen und Zeichen korrekt ändern
                 InetAddress sender = packet.getAddress();
                 char playerSymbol = sender.equals(player1) ? 'X' : 'O';
- 
+
                 //Makes sure that the move comes from the right player
                 if ((currentPlayer && sender.equals(player2)) || (!currentPlayer && sender.equals(player1))) {
                     int move = Integer.parseInt(msg);
                     // nur setzen, wenn Feld frei
                     if (move >= 1 && move <= 9) {
                         if (board[move - 1] != 'X' && board[move - 1] != 'O') {
- 
+
                             board[move - 1] = playerSymbol;
                             sendCurrentBoard(board, player1, player2, socket);
                             int fullCounter = 0;
@@ -151,7 +141,7 @@ public class Server {
                                     board[i - 1] = (char) ('0' + i); // '0' hat den Wert 48
                                 }
                                 break;
- 
+
                             } else if (fullCounter == 9 && !checkWin(board, 'X') && !checkWin(board, 'O')) {
                                 sendMessageToPlayer(player1, socket, "Message: Draw!");
                                 sendMessageToPlayer(player2, socket, "Message: Draw!");
@@ -161,7 +151,7 @@ public class Server {
                                 }
                                 break;
                             }
- 
+
                             if (currentPlayer == false) {
                                 sendMessageToPlayer(player1, socket, "Message: Your Move");
                             } else if (currentPlayer == true) {
@@ -203,22 +193,22 @@ public class Server {
                 byte[] buffer = new byte[1024];
                 String msg;
                 int move;
- 
+
                 //Sends the Message to the player, that it is his turn
                 if (currentPlayer == false) {
                     sendMessageToPlayer(activePlayer, socket, "Message: Your Move");
                 }
- 
+
                 //If the currentPlayer is true (bot), than it makes the botmove and sends it to the player
                 if (currentPlayer) {
                     currentPlayer = false;
                     msg = bot.calculateMove(board);
                     move = Integer.parseInt(msg);
- 
+
                     // nur setzen, wenn Feld frei
                     if (move >= 1 && move <= 9) {
                         if (board[move - 1] != 'X' && board[move - 1] != 'O') {
- 
+
                             board[move - 1] = 'O';
                             String stringBoard = boardToString(board);
                             sendMessageToPlayer(activePlayer, socket, "Message: botmove");
@@ -238,18 +228,18 @@ public class Server {
                     DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                     socket.receive(packet);
                     InetAddress sender = packet.getAddress();
- 
+
                     msg = new String(packet.getData(), 0, packet.getLength()).trim();
                     System.out.println("Received: " + msg);
- 
+
                     //Makes sure that the move comes from the player1
                     if ((!currentPlayer && sender.equals(activePlayer))) {
                         move = Integer.parseInt(msg);
- 
+
                         // nur setzen, wenn Feld frei
                         if (move >= 1 && move <= 9) {
                             if (board[move - 1] != 'X' && board[move - 1] != 'O') {
- 
+
                                 board[move - 1] = 'X';
                                 String stringBoard = boardToString(board);
                                 sendMessageToPlayer(activePlayer, socket, stringBoard);
@@ -286,7 +276,7 @@ public class Server {
             }
         }
     }
- 
+
     /**
      * Handels the different kind of wins/draws
      * @param activePlayer
@@ -307,7 +297,7 @@ public class Server {
             for (int i = 1; i <= 9; i++) {
                 board[i - 1] = (char) ('0' + i); // '0' hat den Wert 48
             }
- 
+
         } else if (fullCounter == 9 && !checkWin(board, 'X') && !checkWin(board, 'O')) {
             sendMessageToPlayer(activePlayer, socket, "Message: Draw!");
             playing = false;
@@ -316,7 +306,7 @@ public class Server {
             }
         }
     }
- 
+
     /**
      * Transforms the board to a String
      * @param board
@@ -328,7 +318,7 @@ public class Server {
                 + " | " + board[8] + "\n";
         return boardStateString;
     }
- 
+
     /**
      * Sends current Board to all Players
      * @param board
@@ -342,16 +332,16 @@ public class Server {
         String boardStateString = "_________\n" + board[0] + " | " + board[1] + " | " + board[2] + "\n" + "--+---+--\n"
                 + board[3] + " | " + board[4] + " | " + board[5] + "\n" + "--+---+--\n" + board[6] + " | " + board[7]
                 + " | " + board[8] + "\n";
- 
+
         System.out.println(boardStateString);
- 
+
         byte[] data = boardStateString.getBytes();
         DatagramPacket board1 = new DatagramPacket(data, data.length, player1, 6970);
         DatagramPacket board2 = new DatagramPacket(data, data.length, player2, 6970);
         socket.send(board1);
         socket.send(board2);
     }
- 
+
     /**
      * Checks for a win on the board
      * @param board
@@ -365,7 +355,7 @@ public class Server {
             return true;
         if (board[6] == player && board[7] == player && board[8] == player)
             return true;
- 
+
         // Spalten
         if (board[0] == player && board[3] == player && board[6] == player)
             return true;
@@ -373,16 +363,16 @@ public class Server {
             return true;
         if (board[2] == player && board[5] == player && board[8] == player)
             return true;
- 
+
         // Diagonalen
         if (board[0] == player && board[4] == player && board[8] == player)
             return true;
         if (board[2] == player && board[4] == player && board[6] == player)
             return true;
- 
+
         return false;
     }
- 
+
     /**
      * Sends Messages to certain Players
      * @param Player
@@ -395,7 +385,7 @@ public class Server {
         DatagramPacket Message = new DatagramPacket(data, data.length, Player, 6970);
         socket.send(Message);
     }
- 
+
     public static void startTimer(boolean player1Ready, boolean player2Ready) {
         if ((!player1Ready && player2Ready) || (player1Ready && !player2Ready)) {
             new Thread(() -> {
@@ -412,3 +402,4 @@ public class Server {
         }
     }
 }
+ 
