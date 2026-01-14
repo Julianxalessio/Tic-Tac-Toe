af67c2049125662286b02bb12d2866297dbe77eb
atb-julo <julianxalessio@gmail.com>
2026-01-14 15:38:14 +0100

    terminate changed

--- out/production/Tic-Tac-Toe/docs/type-search-index.js
+++ out/production/Tic-Tac-Toe/docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html","k":"18"},{"p":"Server","l":"botClass"},{"p":"Server","l":"botNetwork"},{"p":"Clients","l":"Player1"},{"p":"Clients","l":"Player2"},{"p":"Server","l":"Server"}];updateSearchResults();
+typeSearchIndex = [{"l":"Alle Klassen und Schnittstellen","u":"allclasses-index.html","k":"18"},{"p":"ServerPackage","l":"BotClass"},{"p":"ServerPackage","l":"BotNetwork"},{"p":"ServerPackage","l":"MainGame"},{"p":"Clients","l":"Player1"},{"p":"ServerPackage","l":"Server"}];updateSearchResults();
